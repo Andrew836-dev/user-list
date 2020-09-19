b3108cf6030447b98b42aacdb84279cffe8bf42d
Andrew Gray <andrew.aj.gray@gmail.com>
2020-09-20 09:26:15 +0930

    added placeholder sort function

--- src/API.js
+++ src/API.js
@@ -1,7 +1,7 @@
 // import axios from "axios";
 
 const API = {
-  getUsers: () => {
+  getUsers: async () => {
     return [
       {
         id: 0,
@@ -22,6 +22,21 @@ const API = {
         email: "Julia@world.com"
       }
     ]
+  },
+  getUsersSortedBy: async field => {
+    return API.getUsers().then(list => {
+        let sorted;
+        do {
+          sorted = true;
+          for (let i = 0; i < list.length; i++) {
+            if (list[i + 1] && list[i][field] > list[i + 1][field]) {
+              sorted = false;
+              [list[i], list[i + 1]] = [list[i + 1], list[i]]
+            }
+          }
+        } while (!sorted);
+        return list;
+      });
   }
 }
 

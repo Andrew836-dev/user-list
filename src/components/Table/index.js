60df9aa670a984a1604f2b73fc51680798d83ecd
Andrew Gray <andrew.aj.gray@gmail.com>
2020-09-20 09:27:14 +0930

    set up placeholder table

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,29 +1,7 @@
 import React from "react";
-import API from "../../API";
-import SortBar from "../SortBar";
 
-class Table extends React.Component {
-  state = {
-    sortedBy: "Name"
-  }
-
-  render(props) {
-    return <div>
-      <h1>Hello Table</h1>
-      <SortBar sortedBy={this.state.sortedBy} handleClick={this.handleClick} />
-      {this.getSortedUsers().map(user => {
-        return (<p key={user.id}>{user.name} : {user.role} : {user.email}</p>)
-      })}
-    </div>
-  }
-  
-  handleClick = event => {
-    event.preventDefault();
-    this.setState({sortedBy: event.target.id});
-  }
-  getSortedUsers = () => {
-    return API.getUsers();
-  }
+function Table(props) {
+  return props.data.map(user => (<p key={user.id}>{user.name} : {user.role} : {user.email}</p>))
 }
 
 export default Table;

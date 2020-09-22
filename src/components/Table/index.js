344b0e5532356f981fa94d4515eb5a96fdc521ac
Andrew Gray <andrew.aj.gray@gmail.com>
2020-09-22 13:39:22 +0930

    finished table component, will generate all rows dynamically

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,115 +1,17 @@
 import React from "react";
-import { useTable, useSortBy } from "react-table";
 
 function Table(props) {
-  const columns = React.useMemo(() =>
-    [
-      {
-        Header: 'Employee Table',
-        columns: [
-          {
-            Header: 'ID',
-            accessor: 'id',
-            id: 'id',
-            sortType: 'basic',
-          },
-          {
-            Header: 'Name',
-            id: 'name',
-            accessor: 'name',
-          },
-          {
-            Header: 'Role',
-            accessor: 'role',
-            id: 'role',
-          },
-          {
-            Header: 'Email',
-            accessor: 'email',
-            id: 'email',
-          }
-        ]
-      },
-    ],
-    []
-  );
-
-  const data = React.useMemo(() => props.data, [props.data]);
-
-  const initialSort = React.useMemo(() =>
-    [{ id: "role", desc: true }]
-    , []);
-
-  const tableInstance = useTable({
-    columns: columns,
-    data: data,
-    initalState: {
-      sortBy: initialSort,
-      hiddenColumns: ["role", "id"],
-    },
-  }, useSortBy)
-
-  const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    rows,
-    prepareRow,
-  } = tableInstance
-
-  if (!props.data[0]) {
-    return <h1>Loading</h1>
-  } else {
-    return (
-      // apply the table props
-      <table {...getTableProps()}>
-        <thead>
-          {// Loop over the header rows
-            headerGroups.map(headerGroup => (
-              // Apply the header row props
-              <tr {...headerGroup.getHeaderGroupProps()}>
-                {// Loop over the headers in each row
-                  headerGroup.headers.map(column => (
-                    // Apply the header cell props
-                    <th {...column.getHeaderProps(column.getSortByToggleProps())}>
-                      {// Render the header
-                        column.render('Header')}
-                      {column.isSorted
-                        ? column.isSortedDesc
-                          ? ' 🔽'
-                          : ' 🔼'
-                        : ''}
-                    </th>
-                  ))}
-              </tr>
-            ))}
-        </thead>
-        {/* Apply the table body props */}
-        <tbody {...getTableBodyProps()}>
-          {// Loop over the table rows
-            rows.map(row => {
-              // Prepare the row for display
-              prepareRow(row)
-              return (
-                // Apply the row props
-                <tr {...row.getRowProps()}>
-                  {// Loop over the rows cells
-                    row.cells.map(cell => {
-                      // Apply the cell props
-                      return (
-                        <td {...cell.getCellProps()}>
-                          {// Render the cell contents
-                            cell.render('Cell')}
-                        </td>
-                      )
-                    })}
-                </tr>
-              )
-            })}
-        </tbody>
-      </table>
-    )
-  }
+  return <>
+    <tbody>
+      {props.data.map(row => {
+        return <tr key={row.id}>
+          {props.headers.map(column => {
+            return <td key={column.concat(row.id)}>{row[column]}</td>
+          })}
+        </tr>
+      })}
+    </tbody>
+  </>
 }
 
 export default Table;

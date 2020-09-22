aaedf356401faaaef6cde9fd038c7b272c795c23
Andrew Gray <andrew.aj.gray@gmail.com>
2020-09-22 23:07:53 +0930

    added randomuser.me API call

--- src/API.js
+++ src/API.js
@@ -1,34 +1,41 @@
-// import axios from "axios";
+import axios from "axios";
 
 const API = {
   getUsers: async () => {
-    return [
-      {
-        id: 0,
-        firstName: "Andrew",
-        lastName: "Pumpkin",
-        role: "Healer",
-        email: "jella@world.com"
-      },
-      {
-        id: 1,
-        firstName: "Kelly",
-        lastName: "Eater",
-        role: "Tank",
-        email: "jell@world.com"
-      },
-      {
-        id: 2,
-        firstName: "Julia",
-        lastName: "Peters",
-        role: "Accountant",
-        email: "jElly@aorld.com"
-      }
-    ]
-  },
-  getUsersSortedBy: async (field = "id") => {
-    return API.getUsers().then(list => {
-      return API.sortArray(list, field)
+    // return [
+    //   {
+    //     id: 0,
+    //     firstName: "Andrew",
+    //     lastName: "Pumpkin",
+    //     role: "Healer",
+    //     email: "jella@world.com"
+    //   },
+    //   {
+    //     id: 1,
+    //     firstName: "Kelly",
+    //     lastName: "Eater",
+    //     role: "Tank",
+    //     email: "jell@world.com"
+    //   },
+    //   {
+    //     id: 2,
+    //     firstName: "Julia",
+    //     lastName: "Peters",
+    //     role: "Accountant",
+    //     email: "jElly@aorld.com"
+    //   }
+    // ]
+    return axios.get("https://randomuser.me/api/?inc=name,email,phone&results=10")
+    .then(({data}) => {
+      return data.results.map((person, index) => {
+        return {
+          id: index,
+          firstName: person.name.first,
+          lastName: person.name.last,
+          email: person.email,
+          phone: person.phone
+        }
+      });
     });
   },
 }
